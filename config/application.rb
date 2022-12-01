69f5070932a17efa844fa811e80faef98febb104
Raphael Bellec <raphael.bellec@gmail.com>
2022-12-01 09:45:23 +0100

    Add a dedicated directory

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Aoc2022
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.eager_load_paths << Rails.root.join("app/daily_problems")
   end
 end
