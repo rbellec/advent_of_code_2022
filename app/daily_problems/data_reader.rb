db411cf3cb1a9d730fafbd5a6810319f5da3f66e
Raphael Bellec <raphael.bellec@gmail.com>
2022-12-01 10:59:06 +0100

    forgot AOC files can not be directly read online. Add some boilerplate.

---
+++ app/daily_problems/data_reader.rb
@@ -0,0 +1,23 @@
+class DataReader
+
+  attr_reader :day, :problem, :extention
+
+  def initialize(day: ,problem:, extention: "txt")
+    @day = day
+    @problem = problem
+    @extention = extention
+  end
+
+  def content
+    @content ||= read_file
+  end
+
+  def file_path
+    filename = "data_day_#{day}_#{problem}.#{extention}"
+    Rails.root.join('problems_data').join(filename)
+  end
+
+  def read_file
+     File.readlines(file_path)
+  end
+end
