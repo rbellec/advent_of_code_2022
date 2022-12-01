8848f91312e76a23324f9c2ab7397684d4e9e802
Raphael Bellec <raphael.bellec@gmail.com>
2022-12-01 10:34:39 +0100

    Day 1, Solution 1

--- app/daily_problems/day_01.rb
+++ app/daily_problems/day_01.rb
@@ -1,5 +1,37 @@
 class Day01
+
+  ELVES_CALORIES = [
+    "1000",
+    "2000",
+    "3000",
+    "",
+    "4000",
+    "",
+    "5000",
+    "6000",
+    "",
+    "7000",
+    "8000",
+    "9000",
+    "",
+    "1000",
+  ]
+
   def call
-    puts "Setup OK"
+    # Find the elve carrying the most calories
+    elve, calories = calory_per_elve.max_by { |_elve, calories| calories }
+    puts "Elve #{elve} is carrying #{calories} calories"
+  end
+
+  def calory_per_elve
+    inventory_per_elve.transform_values{|inventory| inventory.sum}
+  end
+
+  def inventory_per_elve
+    # Group calories by elves
+    ELVES_CALORIES.chunk{|l| !l.blank?}
+    .filter{|g| g.first} # supress empty lines
+    .each_with_index.map{|chunk, elve_index| [elve_index, chunk.last.map(&:to_i)]}
+    .to_h
   end
 end
