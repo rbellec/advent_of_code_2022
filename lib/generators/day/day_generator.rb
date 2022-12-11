9c0538b325ce59f9c18f52e73e472a77c8dc0045
Raphael Bellec <raphael.bellec@gmail.com>
2022-12-11 11:45:11 +0100

    Start a generator for daily file creation:

---
+++ lib/generators/day/day_generator.rb
@@ -0,0 +1,11 @@
+class DayGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  def copy_day_file
+    day = name.to_i
+    new_code_file = "app/daily_problems/day_%02d.rb" % day
+
+    copy_file "day.rb", new_code_file
+    gsub_file new_code_file, '#DailyClassName', "Day%02d" % day
+  end
+end
---
+++ lib/generators/day/templates/day.rb
@@ -0,0 +1,38 @@
+class #DailyClassName
+  extend DailyProblemHelpers
+
+  TEST_DATA_1 = <<~DATA
+    .
+  DATA
+
+  TEST_DATA_2 = <<~DATA
+    .
+  DATA
+
+  TEST_DATA = TEST_DATA_1
+
+  def self.call(dataset: :test)
+    day_solver = new(open_dataset(dataset: dataset))
+    day_solver.problem_1
+  end
+
+  def initialize(data_stream)
+    read(data_stream)
+  end
+
+  def read(stream)
+    stream.each_line do |line|
+      case line.chomp!
+      when /.../
+      else      end
+    end
+  end
+
+  def problem_1
+  end
+
+  def problem_2
+  end
+
+  # attr_reader :
+end
