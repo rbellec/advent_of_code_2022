433479b827bbdcf290d2023a4bd42a00cd8ee5dd
Raphael Bellec <raphael.bellec@gmail.com>
2022-12-12 10:16:20 +0100

    Update generator to add data file

--- lib/generators/day/day_generator.rb
+++ lib/generators/day/day_generator.rb
@@ -5,7 +5,8 @@ class DayGenerator < Rails::Generators::NamedBase
     day = name.to_i
     new_code_file = "app/daily_problems/day_%02d.rb" % day
 
-    copy_file "day.rb", new_code_file
-    gsub_file new_code_file, '#DailyClassName', "Day%02d" % day
+    copy_file("day.rb", new_code_file)
+    gsub_file(new_code_file, '#DailyClassName', "Day%02d" % day)
+    create_file("problems_data/data_day_%02d_1.txt" % day, "")
   end
 end
