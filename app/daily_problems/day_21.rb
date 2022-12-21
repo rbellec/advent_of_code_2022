b50fd726ae1fd9277b5577e375bbf68a480ad5e0
Raphael Bellec <raphael.bellec@gmail.com>
2022-12-21 16:46:58 +0100

    Day21: I tried to parse with a grammar to build an other Treetop example but did not have time.
    I wanted then to build a system doig up/down tree traversals, but I heard about the complex trick.
    I wish I could have thougt about it without hearing, but I wanted to try it and it worked
    after modifying very few lines

---
+++ app/daily_problems/day_21.rb
@@ -0,0 +1,97 @@
+class Day21
+  extend DailyProblemHelpers
+
+  TEST_DATA = <<~DATA
+    root: pppw + sjmn
+    dbpl: 5
+    cczh: sllz + lgvd
+    zczc: 2
+    ptdq: humn - dvpt
+    dvpt: 3
+    lfqf: 4
+    humn: 5
+    ljgn: 2
+    sjmn: drzm * dbpl
+    sllz: 4
+    pppw: cczh / lfqf
+    lgvd: ljgn * ptdq
+    drzm: hmdt - zczc
+    hmdt: 32
+  DATA
+
+  def self.call(level=1,problem = false)
+    dataset = problem ? :problem : :test
+    day_solver = new(open_dataset(dataset: dataset))
+    case level
+    when 1
+      day_solver.problem_1
+    when 2
+      day_solver.problem_2
+    end
+  end
+
+  def initialize(data_stream)
+    read(data_stream)
+  end
+
+  def read(stream)
+    @monkey_definitions = stream.each_line.each_with_index.map do |line, index|
+      case line.chomp!
+      when /(\w+)\: (\d+)/
+        [$1, {type: :scream, value: $2.to_i, num: index + 1}] # Why do I think we will need monkey index ?
+      when /(\w+)\: (\w+) ([\+\-\*\/]) (\w+)/
+        [$1, {type: :operation, lhs:$2, operator:$3, rhs:$4, value: nil, num: index + 1}] # Why do I think we will need monkey index ?
+      else
+        puts "Line not matched."
+      end
+    end.to_h
+  end
+
+  def monkey_value(name)
+    raise ("Asking a non exitent monkey. They all scream together incoherent numbers") if !monkey_definitions.has_key?(name)
+    existing_value = monkey_definitions.dig(name, :value)
+    return existing_value if existing_value
+
+    monkey = monkey_definitions[name]
+
+    raise("Monkey error, throwing barrels everywhere in tribute to donkey kong") unless monkey[:type] == :operation
+
+    # Not: infinite loop in cycles in monkey definitions.
+    monkey_value = case monkey[:operator]
+    when "+"
+      monkey_value(monkey[:lhs]) + monkey_value(monkey[:rhs])
+    when "-"
+      monkey_value(monkey[:lhs]) - monkey_value(monkey[:rhs])
+    when "*"
+      monkey_value(monkey[:lhs]) * monkey_value(monkey[:rhs])
+    when "/"
+      # I don't know what monkey could do in case of a divide by zero and I am not sure I would like to !
+      monkey_value(monkey[:lhs]) / monkey_value(monkey[:rhs])
+    end
+  end
+
+  # I initially prepared a up/down solver on an operation tree before hearing "With complex numbers..."
+
+  def problem_1
+    monkey_value("root")
+  end
+
+  def problem_2
+    monkey_definitions["humn"][:value] = Complex(0,1)
+    root = monkey_definitions["root"]
+    rhs = monkey_value(root[:rhs])
+    lhs = monkey_value(root[:lhs])
+
+    if rhs.is_a? Complex
+      real = lhs
+      complex = rhs
+    else
+      real = rhs
+      complex = lhs
+    end
+
+    solution = (real - complex.real) / complex.imaginary
+  end
+
+  attr_reader :monkey_definitions
+end
---
+++ app/daily_problems/grammars/day_21_grammar_test.rb
@@ -0,0 +1,86 @@
+# # How to write a treetop parser II
+# I just follow the same process as described in Day 19
+
+require "pp"
+require "treetop"
+require "byebug"
+
+# Load the grammar
+Treetop.load "day_21_grammar"
+
+# Keep sample data, I try to keep different forms of text to avoid the trap of parsing only one. Here the differences are spaces only.
+
+TEST_DATA = <<~DATA
+  root: pppw + sjmn
+  dbpl: 5
+  ptdq: humn - dvpt
+  ptdq: humn / dvpt
+  ptdq: humn * dvpt
+DATA
+
+# Test framework used for this article :)
+def test(name, bool_value)
+  print(bool_value ? " . " : "error #{name}")
+end
+
+# First parser tests. The goal here is to test one by one, starting with the simplest, all rules.
+# a simple rule not working as expected can be difficult to test from more complex ones. TDD is perfect to
+# speed up this kind of development. You have here the full tests, but I generally work this way:
+# 1. Write one general test for the rule. Just check it parses input.
+# 2. make it works eventually using debugger or output
+# 3. write multiple cases for this rule. You will find only one rule here with multiple cases.
+
+def test_parsers
+  parser = MonkeysMathParser.new
+
+  # in rspec: expect(parser.parse("10", root: :number)).not_to be_nil
+  test "number", parser.parse("10", root: :number)
+  test "name", parser.parse("pppw", root: :name)
+
+  ["pppw + sjmn", "pppw - sjmn", "pppw * sjmn", "pppw / sjmn"].each do |test_string|
+    test "operation", parser.parse(test_string, root: :operation)
+  end
+
+  a = parser.parse("root: pppw + sjmn", root: :monkey)
+    byebug
+  test "monkey_1", parser.parse("root: pppw + sjmn", root: :monkey)
+  test "monkey_2", parser.parse("dbpl: 5", root: :monkey)
+  puts "end"
+end
+
+# Now you are confident your parser is working on some test data. This is the time to build result values (AST or anything you need).
+# This is also an iterative process where I like to stay free of the final representation while I move to more complex parsers.
+# I often decide to change sub rules representations of result and iterate while writing theses tests.
+# Note that I have not done enough parser to find a proper convention for rules added methods names. As much as I do not like the
+# repetition of words, this is what I ended with. Also I sometimes do not undersand why I can not access text_values in terminal rules and
+# did not spend enough time on this yet, you will find that `number` rules result here has to be handled in above rules.
+
+def test_parsers_return_values
+  parser = MonkeysMathParser.new
+
+  # in rspec: expect(parser.parse("10", root: :number)).not_to be_nil
+  test "number", parser.parse("10", root: :number)
+  test("name", parser.parse("pppw", root: :name).text_value == "pppw")
+
+  test("operation",
+    parser.parse("pppw + sjmn", root: :operation).operation == {operator: "+", lhs: "pppw" , rhs: "sjmn"}
+  )
+
+  test("monkey",
+    parser.parse("root: pppw + sjmn", root: :monkey).monkey == ["root", {operator: "+", lhs: "pppw" , rhs: "sjmn"}]
+  )
+
+  # pp parser.parse(TEST_DATA, root: :blueprints).blueprints
+  puts "end rv"
+end
+
+# Then I try to parse the whole file and check it works and give a usable result.
+def test_parse_problem_file
+  text = File.read("../../../problems_data/data_day_19_1.txt")
+  parser = BluePrintsParser.new
+  pp parser.parse(text).blueprints
+end
+
+test_parsers
+test_parsers_return_values
+# test_parse_problem_file
